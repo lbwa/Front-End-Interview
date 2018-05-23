a73abed9e9977d11703ba8601027c7b17ecd6029
Bowen <mr_lbw@outlook.com>
2018-05-24 00:10:18 +0800

    optimize: add comment about deploying site

--- commit.sh
+++ commit.sh
@@ -6,6 +6,15 @@ git push -u origin master
 
 echo '-- >> 分支 master 提交完成 << --'
 
+# git checkout gh-pages
+# cp -r _book/* .
+# git add .
+# git commit -m "$1"
+# git push -u origin gh-pages
+# git checkout master
+
+# echo '-- >> 分支 gh-pages 部署完成 << --'
+
 # 于 git bash 中输入 ./deploy.sh 'info about commit'
 # 或者输入 sh deploy.sh 'info about commit'
 
