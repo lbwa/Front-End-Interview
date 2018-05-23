352bdb5e47fbea5c4ce8b60d2cb0a40ef74600c6
Bowen <mr_lbw@outlook.com>
2018-05-23 22:26:15 +0800

    build

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# sh deploy.sh '更新说明'
+git checkout master
+git add .
+git commit -m $1
+git push -u origin master
+git checkout gh-pages
+cp -r _book/* .
+git add .
+git commit -m $1
+git push -u origin gh-pages
+git checkout master
