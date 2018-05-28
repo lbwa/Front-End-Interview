ea38b70c02129585a8b785d6c3004dd229b0743b
Bowen <mr_lbw@outlook.com>
2018-05-28 22:03:31 +0800

    debug:

--- commit.sh
+++ commit.sh
@@ -6,9 +6,8 @@ git push -u origin master
 
 echo '-- >> 分支 master 提交完成 << --'
 
-git checkout gh-pages
-cp -r _book/* .
-whoami
+# git checkout gh-pages
+# cp -r _book/* .
 # git add .
 # git commit -m "$1"
 # git push -u origin gh-pages
