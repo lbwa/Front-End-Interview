9e4f700e448755e906e849d6bd150e6b286d4c44
Bowen <mr_lbw@outlook.com>
2018-05-28 21:31:53 +0800

    debug commit.sh

--- commit.sh
+++ commit.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-git checkout master
-git add .
-git commit -m "$1"
-git push -u origin master
-
-echo '-- >> 分支 master 提交完成 << --'
-
-# git checkout gh-pages
-# cp -r _book/* .
+# git checkout master
 # git add .
 # git commit -m "$1"
-# git push -u origin gh-pages
-# git checkout master
+# git push -u origin master
 
-# echo '-- >> 分支 gh-pages 部署完成 << --'
+# echo '-- >> 分支 master 提交完成 << --'
+
+git checkout gh-pages
+cp -r _book/* .
+git add .
+git commit -m "$1"
+git push -u origin gh-pages
+git checkout master
+
+echo '-- >> 分支 gh-pages 部署完成 << --'
 
 # 于 git bash 中输入 ./deploy.sh 'info about commit'
 # 或者输入 sh deploy.sh 'info about commit'
