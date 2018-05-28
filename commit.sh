8e497e93ded939748ee65bcf3c923aa56dc0b7b3
Bowen <mr_lbw@outlook.com>
2018-05-28 22:23:40 +0800

    debug: commit.sh

--- commit.sh
+++ commit.sh
@@ -6,6 +6,8 @@ git push -u origin master
 
 echo '-- >> 分支 master 提交完成 << --'
 
+gitbook build
+
 # git checkout gh-pages
 # cp -r _book/* .
 # git add .
@@ -13,14 +15,10 @@ echo '-- >> 分支 master 提交完成 << --'
 # git push -u origin gh-pages
 # git checkout master
 
-gitbook build
-cp -r _book docs
-git subtree push --prefix docs origin gh-pages
-rm -rf docs
-echo '-- >> 分支 gh-pages 部署完成 << --'
+# echo '-- >> 分支 gh-pages 部署完成 << --'
 
-# 于 git bash 中输入 ./deploy.sh 'info about commit'
-# 或者输入 sh deploy.sh 'info about commit'
+# ./deploy.sh 'info about commit'
+# sh deploy.sh 'info about commit'
 
 # 在 shell 中区分双引号和单引号
-# 在 master 分支中无法通过 shell 提交 gh-pages 分支修改，提示权限不够，所以暂只有分开提交
+# 在 master 分支中无法通过 shell 复制 commit.sh 本身，提示权限不够，所以暂只有分开提交
--- docs/project-experience/project-design-pattern.md
+++
@@ -1,8 +0,0 @@
-（待拓展到目录）
-# 开闭原则
-
-[开闭原则](../js-essentials/js-async.md)
-
-# 库的拓展性
-
-[原型对象的拓展性](../js-essentials/js-prototype-application.md)
